ccb23491a0cb24cf5fd36c499e6e0fdeb5f03b41
Luis David Herrera Medina <luis.herrerame@cecar.edu.co>
2024-09-15 02:29:13 -0500

    upate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
 
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3307, // VIDA DE MY VIDA
+      port: 3306, // VIDA DE MY VIDA
       //port: 3306,
-      username: 'root',
-      password: 'MR.robot99',
-      database: 'talentohumanosin_posesion', // VIDA DE MY VIDA
-      //database: 'talentohumanosin_posesio', // LUIS 
+      username: 'admin',
+      password: 'admin009',
+      database: 'talentohumanosin_posesio',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
